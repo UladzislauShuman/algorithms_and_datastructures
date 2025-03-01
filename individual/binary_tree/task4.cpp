a7c24a0a614091d52c2c86c68d34c95301aa13a0
UladzislauLinux <fpm.shumanVO@bsu.by>
2025-03-01 22:49:04 +0300

    in process of solving individual for binary_tree

---
+++ individual/binary_tree/task4.cpp
@@ -0,0 +1,220 @@
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+class BinaryTree
+{
+private:
+    struct Node {
+        int data;
+        Node* left;
+        Node* right;
+        int descendantsAmount;
+        Node(int data) : data(data), left(nullptr), right(nullptr), descendantsAmount(0){}
+        Node() : data(0), left(nullptr), right(nullptr), descendantsAmount(0){}
+        void setData(int data) {this->data = data;}
+        int getData() const {return this->data;}
+
+        std::string toString() const {return "Node " + std::to_string(this->data) + " " + std::to_string(this->descendantsAmount);}
+    };
+
+    void insertHelper(Node* node, int data) {
+        if (node->getData() == data) 
+            return;
+        if (data < node->getData()) {
+            if (node->left == nullptr) {
+                node->left = new Node(data);
+                return;
+            }
+            else 
+                this->insertHelper(node->left, data);
+        }
+        if (data > node->getData()) {
+            if (node->right == nullptr){
+                node->right = new Node(data);
+                return;
+            }
+            else
+                this->insertHelper(node->right, data);
+        }
+    }  
+
+    Node* find_min(Node* node) {
+        while (node->left != nullptr) node = node->left;
+        return node;
+    }
+
+    Node* delete_node_helper(Node* node, int data) { // рекурсивно работает
+    // возвращаемое как бы играет роль привязки для предыдущего
+    //  тут как бы поиск
+    // оно не выйдет за пределы 
+        if (node == nullptr) return node; 
+        if (data < node->getData()) {
+            node->left = delete_node_helper(node->left, data);
+            return node;
+        }
+        if (data > node->getData()) {
+            node->right = delete_node_helper(node->right, data);
+            return node;
+        }
+        // если мы оказались в этой точке, то зн что data == node->getData()
+        // прешло время удалять
+        
+        
+        // если только один потомок
+        /*
+            если слева потомка нет, то просто правый ставим на место удаляемого
+        */
+        if (node->left == nullptr) {
+            Node* temp = node->right; 
+            delete node;
+            return temp;
+        }
+        // если мы тут, то -- то у нас есть левый потомок 
+        /*
+            если справа потомка нет, то просто левый ставим на место удаляемого
+        */
+        if (node->right == nullptr) {
+            Node* temp = node->left;
+            delete node;
+            return temp;
+        }
+        // если мы уже и тут оказались, то у нас есть два потомка
+        // для правого удаления нам надо вправо И потом в самое лево
+        Node* temp = find_min(node->right); // т.е. ищем минимальный (самый левый) для правого детёныша
+        node->setData(temp->getData()); // мы не будем как бы менять адреса -- мы просто поменяем менями значения И find_min 
+        node->right = delete_node_helper(node->right, temp->getData()); // удаляем тот самый find_min
+        return node;
+    }
+
+    void deleteBinaryTree(Node* node) {
+        if (node) {
+            deleteBinaryTree(node->left);
+            deleteBinaryTree(node->right);
+            delete node;
+        }
+    }
+
+    // часть индивидуального задания
+    int getHeightHelper(Node* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+    
+        int leftHeight = getHeightHelper(node->left);
+        int rightHeight = getHeightHelper(node->right);
+    
+        // Вычисляем количество потомков для текущего узла:
+        int leftDescendants = (node->left == nullptr) ? 0 : node->left->descendantsAmount + 1;
+        int rightDescendants = (node->right == nullptr) ? 0 : node->right->descendantsAmount + 1;
+        node->descendantsAmount = leftDescendants + rightDescendants;
+    
+        return 1 + std::max(leftHeight, rightHeight);
+    }
+    
+
+    
+public:
+    Node* root;
+    BinaryTree() {
+        this->root = nullptr;
+    }
+    ~BinaryTree() {
+        this->deleteBinaryTree(this->root);
+    }
+
+    void insert(int data)
+    {
+        if (root == nullptr) {
+            root = new Node(data);
+        } else {
+            insertHelper(root, data);
+        }
+    }
+
+    // реализация паттерна Итератор для Прямого Левого Обхода бин-дерева
+    class BinaryTreeIterator {
+    private:
+        std::stack<Node*> nodeStack;  
+        friend class BinaryTree;
+
+        Node* getCurrentNode() {
+            if (!nodeStack.empty()) {
+                return nodeStack.top();
+            }
+            return nullptr;
+        }
+    public:
+        BinaryTreeIterator(Node* root_)  {
+            if (root_) nodeStack.push(root_);
+        }
+        // без метода first по банде-4
+        void next() {
+            if (!nodeStack.empty()) {
+                Node* node = nodeStack.top();
+                nodeStack.pop();
+   
+                if (node->right) {
+                    nodeStack.push(node->right);
+                }
+                if (node->left) {
+                    nodeStack.push(node->left);
+                }
+            }
+        }
+    
+        bool isDone() {
+            return nodeStack.empty();
+        }
+        int getCurrentNodeData() {
+            if (!nodeStack.empty()) {
+                return nodeStack.top()->getData();
+            }
+            return -1;
+        }
+
+        std::string toString() const {
+            if (!nodeStack.empty()) {
+                return nodeStack.top()->toString();
+            }
+            return "\n";
+        }
+    };
+
+    BinaryTreeIterator getBinaryTreeInterator() {
+        return BinaryTreeIterator(this->root);
+    }
+
+    void delete_node(int data) {
+        this->root = this->delete_node_helper(this->root, data);
+    }
+    //часть индивидуального задания
+    int getHeight() {
+        return this->getHeightHelper(this->root);
+    }
+};
+
+int main() {
+    std::ifstream in("input.txt");
+    std::ofstream out("output.txt");
+
+    BinaryTree tree;
+    
+    int temp;
+    while (in >> temp) {
+        tree.insert(temp);
+    }
+    int height = tree.getHeight(); // вызываем, чтобы он прошёлся по дереву один раз И 
+    std::cout << height << "\n";
+    BinaryTree::BinaryTreeIterator it = tree.getBinaryTreeInterator();
+    while (!it.isDone()) {
+        std::cout << it.toString() << "\n";
+        it.next();
+    }
+
+    in.close(); out.close();
+    return 0;
+}
