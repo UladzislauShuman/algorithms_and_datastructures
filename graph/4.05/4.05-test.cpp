9d7bfd4c5b8390cc59e251e5479f8cb390986d12
UladzislauLinux <fpm.shumanVO@bsu.by>
2025-04-02 12:34:17 +0300

    solved task 4.05

--- graph/4.05/4.05-test.cpp
+++ graph/4.05/4.05-test.cpp
@@ -1,28 +1,68 @@
 #include <fstream>
 #include <vector>
-    
-// в матрице смежности для ориентированного графа у нас [i][j] от i к j
- 
+#include <iostream>
+#include <queue>
+
+/*
+рассмотрим первый тест
+7
+0 1 1 0 0 0 0
+0 0 0 0 1 0 0
+0 1 0 1 1 0 0
+0 1 0 0 1 0 0
+0 0 0 0 0 1 0
+0 0 0 1 0 0 0
+0 0 0 0 0 1 0
+
+ответ
+1 2 3 5 4 6 7
+*/
 int main() {
     std::ifstream in("input.txt");
     std::ofstream out("output.txt");
 
-    int n; in >> n;
-    
-    std::vector<int> result(n,0);
+    int n;
+    in >> n;
+    std::vector<std::vector<int>> graph(n);
+
     int temp;
-    int maxId = 1;
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             in >> temp;
-            if (temp == 1)
-                result[j] = result[j] == 0 ? maxId++ : result[j];
+            if (temp == 1) {
+                graph[i].push_back(j);
+            }
         }
     }
-    
-    for (int i  = 0; i < n; ++i)
-        out << result[i] << " ";
 
-    in.close(); out.close();
+    int index = 1;
+    std::vector<int> answer(n, 0);
+
+    for (int i = 0; i < n; ++i) { // нужно, чтобы мы прошли все вершины/компоненты связанности
+        if (answer[i] == 0) {
+            std::queue<int> queue;
+            queue.push(i);
+            answer[i] = index++;
+
+            while (!queue.empty()) {
+                int v = queue.front();queue.pop(); // если выписать очередь с L на R, то 
+
+                for (int j : graph[v]) { // обходим смежные к v
+                    if (answer[j] == 0) { // если не была посещена до этого
+                        answer[j] = index++; // присваиваем индекс
+                        queue.push(j); // и закидываем в очерель
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        out << answer[i] << " ";
+    }
+
+    in.close();
+    out.close();
     return 0;
 }
+
