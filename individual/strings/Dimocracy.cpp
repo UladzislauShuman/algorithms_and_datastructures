fb82a0170d98b34a9884795fd0f0e4fa6e3485db
UladzislauLinux <fpm.shumanVO@bsu.by>
2025-04-17 23:46:03 +0300

    solved 5th individual task

--- individual/strings/Dimocracy.cpp
+++ individual/strings/Dimocracy.cpp
@@ -29,6 +29,8 @@ int main() {
     cout << find_min_substring_length("ABCDEF") << endl;
     cout << find_min_substring_length("AAAAAA") << endl;
     cout << find_min_substring_length("ABCABCABC") << endl;
+    cout << find_min_substring_length("ABCDEFGABCDEFGABCDEFGABCDEFG") << endl;
+    //cout << find_min_substring_length(string(100000,'A') + string(100000,'B')) << endl;
 
     in.close(); out.close();
     return 0;
@@ -40,41 +42,57 @@ int find_min_substring_length(string s) {
     int right = s.size();       
     int result = s.size();
 
+    // для уменьшения коолизии
+    const unsigned long long k1 = 37, mod1 = 1000000007;
+    const unsigned long long k2 = 257, mod2 = 1000000009; // я решил взял простое число поменьше, чтобы было проще считать
+    //чтобы лишний раз не считать
+    vector<unsigned long long> k1_pows(s.size()+1, 1);
+    vector<unsigned long long> k2_pows(s.size()+1, 1);
+    
+    for (int i = 1; i <= s.size(); ++i) {
+        k1_pows[i] = (k1_pows[i-1] * k1) % mod1;
+        k2_pows[i] = (k2_pows[i-1] * k2) % mod2;
+    }
+
     int medium; bool isUnique;
+    unordered_set<unsigned long long> hashes1, hashes2;
+    hashes1.reserve(s.size() - medium + 1);
+    hashes2.reserve(s.size() - medium + 1);
     while (left <= right) {
         medium = left + (right - left) / 2;
         isUnique = true;
-        long long k = 27;
-        long long mod = 1000000000000000003LL;
+        hashes1.clear();
+        hashes2.clear();
 
-        long long k_power = 1;
-        for (int i = 0; i < medium - 1; ++i)  // вот здесь я считаю k^(medium - 1)
-            k_power = k_power * k % mod;
-        
-        unordered_set<long long> hashes;
-        long long temp_hash = 0;
+        unsigned long long temp_hash1 = 0, temp_hash2 = 0;
         
         // сама проверка начинается тут
         for (int i = 0; i < medium; ++i) { // вычисляем перфикс 
-            temp_hash = (temp_hash * k + s[i]) % mod;
+            temp_hash1 = (temp_hash1 * k1 + s[i]) % mod1;
+            temp_hash2 = (temp_hash2 * k2 + s[i]) % mod2;
         }
-        hashes.insert(temp_hash);
+        hashes1.insert(temp_hash1);
+        hashes2.insert(temp_hash2);
 
-        for (int i = medium; i < s.size(); ++i) {  // исправлено medium < на i <
-            temp_hash = (temp_hash - s[i - medium] * k_power % mod + mod) % mod; // удаляем букву левую
-            // + mod % mod  = + 0, но -- мы тем самым избавляемся от возможных отрицательных значений
-            temp_hash = (temp_hash * k + s[i]) % mod;
+        for (int i = medium; i < s.size(); ++i) { 
+            temp_hash1 = (temp_hash1 - s[i - medium] * k1_pows[medium-1] % mod1 + mod1) % mod1;
+            temp_hash1 = (temp_hash1 * k1 + s[i]) % mod1;
 
-            if (hashes.count(temp_hash)) { // был ли уже до этого такой же хеш?
+            temp_hash2 = (temp_hash2 - s[i - medium] * k2_pows[medium-1] % mod2 + mod2) % mod2;
+            temp_hash2 = (temp_hash2 * k2 + s[i]) % mod2;
+
+
+            if (hashes1.count(temp_hash1) && hashes2.count(temp_hash2)) { // был ли уже до этого такой же хеш?
                 isUnique = false;
                 break;
             }
-            hashes.insert(temp_hash);
+            hashes1.insert(temp_hash1);
+            hashes2.insert(temp_hash2);
         }
         //
 
         //проверяем, что произашло за эту итерацию поиска
-        if (isUnique == true) { // если все уникальны 
+        if (isUnique) { // если все уникальны 
             result = medium; // запоминаем результат
             right = medium - 1; // сужаем правую границу (теперь right исключается)
         } else {
